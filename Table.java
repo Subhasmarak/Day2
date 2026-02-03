708af611024d32e4d14b6debde50f6620ac1c5d2
Subhasmarak <subhasmarakdash190@gmail.com>
2026-02-03 11:51:41 +0530

    ChangedTable

--- Table.java
+++ Table.java
@@ -1,10 +1,11 @@
+package Day2;
 import java.util.Scanner;
 class Table{
     public static void main(String arg[]){
         Scanner sc=new Scanner(System.in);
         int n=sc.nextInt();
         for(int i=1;i<=10;i++){
-            System.out.println(n*i);
+            System.out.println(n+"x"+i+"="+(n*i));
         }
     }
 }
